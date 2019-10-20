472e61552f663062b7f1444493310a395f38b651
Stefan Milic <stefan.milic@gn.org>
2019-10-20 21:00:08 +0200

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
+  const [count] = useState();
+  let a = '';
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Learn React {a}
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
